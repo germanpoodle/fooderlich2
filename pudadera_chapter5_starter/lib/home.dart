90aa42513360793841bb3c1189fdb7c1b439cc78
shuakyle <klynejoshua13@gmail.com>
2024-02-28 12:02:49 +0800

    push "folder"

--- pudadera_chapter5_starter/lib/home.dart
+++ pudadera_chapter5_starter/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,10 +13,9 @@ class HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with ExploreScreen
-    Container(color: Colors.white),
+    ExploreScreen(),
     // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
+    RecipesScreen(),
     Container(color: Colors.blue),
   ];
 
