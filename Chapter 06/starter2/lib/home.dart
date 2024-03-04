bded9fa43ac7ffcf1cfb6a6886877f7533411c06
shuakyle <klynejoshua13@gmail.com>
2024-03-04 11:22:51 +0800

    add screen

--- Chapter 06/starter2/lib/home.dart
+++ Chapter 06/starter2/lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
 import 'screens/grocery_screen.dart';
+import 'screens/empty_grocery_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,7 +17,7 @@ class HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
     RecipesScreen(),
-    // TODO: Replace with grocery screen
+    const GroceryScreen(),
     Container(color: Colors.blue),
   ];
 
